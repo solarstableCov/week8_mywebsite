b6c24093c1724f362d230777f4a8ca708e4b21cb
Resume/Coventry University Cybersecurity Bootcamp <you@example.com>
2022-12-22 12:06:27 -0800

    added files

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('house/', include('house.urls')),
+    path('', include('house.urls')),
     path('admin/', admin.site.urls),
 ]
